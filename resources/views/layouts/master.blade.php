880d4d71e741ad2d47fe3d7252ff93091f3cabb5
Ibraheem Moosa <ibraheemmoosa1347@gmail.com>
2016-05-09 07:05:11 +0600

    added css, js files Home.html correctly loading

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -3,20 +3,56 @@
 
 <head>
     <meta charset="UTF-8">
-    <title>
-        @yield('title')
-    </title>
+    <title>@yield('title')</title>
 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css">
-    
+    <link rel="stylesheet" href="css/datepicker.css"> 
 
 </head>
 
 <body>
+    <div style="height:100px">
+        <nav class="navbar navbar-inverse navbar-fixed-top" style="background-color:#334d4d" role="navigation" id="myheader">
+            <div class="container">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="#">Home</a>
+                </div>
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                
+                <div style="float:left">
+                    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                        <ul class="nav navbar-nav">
+                            <li> <a href="#"> All Restaurants </a> </li>
+                            <li> <a href="#"> How To Book </a> </li>
+                        </ul>
+                    </div>
+                </div>
+                
+                <div style="float:right">
+                    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                        <ul class="nav navbar-nav">
+                            <li> <a href="#"> Log In </a> </li>
+                            <li> <a href="#"> Sign Up </a> </li>
+                        </ul>
+                    </div>
+                </div>
+                <!-- /.navbar-collapse -->
+            </div>
+            <!-- /.container -->
+        </nav>
+    </div>
+    
     @yield('content')
 
-    <div id="footer" style="background-color:black">
+    <div id="footer" style="background-color:white">
         <nav class="navbar navbar-bottom" role="navigation">
             <div class="container">
                 <div style="float:right">
@@ -36,6 +72,19 @@
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script> 
+    <script src="js/jquery-1.9.1.min.js"></script>
+    <script src="js/bootstrap-datepicker.js"></script>
+    <script type="text/javascript">
+        // When the document is ready
+         $(document).ready(function () {
+                     
+            $('#example1').datepicker({
+                    format: "dd/mm/yyyy"
+            });  
+                                                                     
+        });
+    </script>
+
 </body>
 
 </html>
