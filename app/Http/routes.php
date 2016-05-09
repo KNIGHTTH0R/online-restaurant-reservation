a4fa46fc947595604ebda21fb831d8f6ce5ccd14
Ibraheem Moosa <ibraheemmoosa1347@gmail.com>
2016-05-09 19:04:47 +0600

    login register generated by artisan auth:make master layout is ours, but none extends it, home overwritten by artisan

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,41 +15,15 @@ Route::get('/', function () {
     return view('home');
 });
 
-Route::get('/restaurants', function () {
-    return "this will show list of all the restaurants";
-});
 
-Route::get('/about', function () {
-    return "This is going to be our about us page";
-});
 
-Route::get('/howto', function() {
-    return "This is going to be our how to book page";
-});
 
-Route::get('/FAQ', function() {
-    return "This is going to be our FAQ page";
-});
 
-Route::get('termsofuse', function() {
-    return "This is going to be our Terms of use page";
-});
 
-Route::get('policy', function() {
-    return "This is going to be our policy page";
-});
 
-Route::get('contactus', function() {
-    return "This is going to be our contact us page";
-});
 
-Route::get('login', function() {
-    return "This is going to be our login page";
-});
 
-Route::get('signup', function() {
-    return "This is going to be our signup page";
-});
+
 
 Route::auth();
 
