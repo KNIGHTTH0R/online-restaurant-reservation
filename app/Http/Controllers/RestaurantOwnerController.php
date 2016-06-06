5ae90d20fb492c6e60de6bb0940e7fcd65eceb15
Ibraheem Moosa <ibraheemmoosa1347@gmail.com>
2016-06-06 14:31:27 +0600

    Non Owner almost cannot change restaurant data

--- app/Http/Controllers/RestaurantOwnerController.php
+++ app/Http/Controllers/RestaurantOwnerController.php
@@ -13,6 +13,10 @@ use Auth;
 class RestaurantOwnerController extends Controller
 {
     //
+    public function __construct()
+    {
+	$this->middleware('auth');
+    }
     public function showAddRestaurant()
     {
         if(\Auth::check() && \Auth::user()->user_type == 1)
@@ -26,98 +30,175 @@ class RestaurantOwnerController extends Controller
     }
     public function showUpdateRestaurant($id)
     {
-	$rest = Restaurant::find($id);
-	$tables = RestaurantTable::where('restaurant_id', '=', $id)->orderBy('capacity', 'asc')->orderBy('booking_fee', 'asc')->get();
-	$food_menus = Food_Menu::where('restaurant_id', '=', $id)->get();
-	return view('restaurantOwner.restaurant_info_update', ['restaurants' => $rest, 'restaurant_tables' => $tables, 'food_menus' => $food_menus]);
+	if(\Auth::check() && \Auth::user()->user_type == 1)
+	{
+	    $rest = Restaurant::find($id);
+	    if($rest->owner_id != Auth::user()->id)
+	    {
+		return redirect('/');
+	    }
+	    $tables = RestaurantTable::where('restaurant_id', '=', $id)->orderBy('capacity', 'asc')->orderBy('booking_fee', 'asc')->get();
+	    $food_menus = Food_Menu::where('restaurant_id', '=', $id)->get();
+	    return view('restaurantOwner.restaurant_info_update', ['restaurants' => $rest, 'restaurant_tables' => $tables, 'food_menus' => $food_menus]);
+	}
+	else
+	{
+	     return redirect('/login');
+	}	    
     }
     public function updateRestaurant(Request $req, $id)
     {
-	$rest = Restaurant::find($id);
-	$rest->location = $req->input('location');
-	$rest->email = $req->input('email');
-	$rest->contact_number = $req->input('contact');
-	$rest->website = $req->input('website');
-	$rest->description = $req->input('desc');
-	$rest->save();
-	return redirect('restaurant_info_update/'.$id);
+	if(\Auth::check() && \Auth::user()->user_type == 1)
+	{
+	    $rest = Restaurant::find($id);
+	    if($rest->owner_id != Auth::user()->id)
+	    {
+		return redirect('/');
+	    }
+
+	    $rest->location = $req->input('location');
+	    $rest->email = $req->input('email');
+	    $rest->contact_number = $req->input('contact');
+	    $rest->website = $req->input('website');
+	    $rest->description = $req->input('desc');
+	    $rest->save();
+	    return redirect('restaurant_info_update/'.$id);
+	}
+	else
+	{
+	     return redirect('/login');
+	}
     }
     public function storeRestaurant(Request $req)
     {
-        $restaurant = new Restaurant();
-        $restaurant->name = $req->input('name');
-        $restaurant->location = $req->input('location');
-        $restaurant->email = $req->input('email');
-        $restaurant->contact_number = $req->input('Contact');
-        $restaurant->website = $req->input('website');
-	$restaurant->description = $req->input('description');
-	$restaurant->owner_id = Auth::user()->id;
-	$restaurant->save();
-	if($req->hasFile('image') && $req->file('image')->isValid()){
-	    $image_file = $req->file('image');
-	    $req->file('image')->move('img/', $restaurant->id.'_'.$req->file('image')->getClientOriginalName());
-	    $restaurant->img_name = $restaurant->id.'_'.$req->file('image')->getClientOriginalName(); 
-	}
-	$this->validate($req, [
+	if(\Auth::check() && \Auth::user()->user_type == 1)
+	{
+	    $restaurant = new Restaurant();
+	    $restaurant->name = $req->input('name');
+	    $restaurant->location = $req->input('location');
+	    $restaurant->email = $req->input('email');
+	    $restaurant->contact_number = $req->input('Contact');
+	    $restaurant->website = $req->input('website');
+	    $restaurant->description = $req->input('description');
+	    $restaurant->owner_id = Auth::user()->id;
+	    $restaurant->save();
+	    if($req->hasFile('image') && $req->file('image')->isValid()){
+		$image_file = $req->file('image');
+		$req->file('image')->move('img/', $restaurant->id.'_'.$req->file('image')->getClientOriginalName());
+		$restaurant->img_name = $restaurant->id.'_'.$req->file('image')->getClientOriginalName(); 
+	    }
+	    $this->validate($req, [
 		'name' => 'required',
 		'Contact' => 'required|integer'
-	]) ;
-	$restaurant->save();
-	return redirect('/account');
+	    ]) ;
+	    $restaurant->save();
+	    return redirect('/account');
+	}
+	else
+	{
+	     return redirect('/login');
+	}
     }
     public function updateFoodMenu(Request $req, $menu_id)
     {
-	$food_menu = Food_Menu::find($menu_id);
-	$food_menu->name = $req->menu_name;
-	$food_menu->price = $req->menu_price;
-	$food_menu->category = $req->menu_category;
-	if($req->hasFile('menu_image') && $req->file('menu_image')->isValid()){
-	    $image_file = $req->file('menu_image');
-	    $req->file('menu_image')->move('img/', $food_menu->id.'_'.$req->file('menu_image')->getClientOriginalName());
-	    $food_menu->img_name = $food_menu->id.'_'.$req->file('menu_image')->getClientOriginalName(); 
+	if(\Auth::check() && \Auth::user()->user_type == 1)
+	{
+	    $food_menu = Food_Menu::find($menu_id);
+	    $rest = Restaurant::find($food_menu->restaurant_id);
+	    if($rest->owner_id != Auth::user()->id)
+	    {
+		return redirect('/');
+	    }
+	    $food_menu->name = $req->menu_name;
+	    $food_menu->price = $req->menu_price;
+	    $food_menu->category = $req->menu_category;
+	    if($req->hasFile('menu_image') && $req->file('menu_image')->isValid()){
+		$image_file = $req->file('menu_image');
+		$req->file('menu_image')->move('img/', $food_menu->id.'_'.$req->file('menu_image')->getClientOriginalName());
+		$food_menu->img_name = $food_menu->id.'_'.$req->file('menu_image')->getClientOriginalName(); 
+	    }
+	    $food_menu->save();
+	    return redirect()->back();
 	}
-	$food_menu->save();
-	return redirect()->back();
+	else
+	{
+	     return redirect('/login');
+	}
     }
     public function addFoodMenu(Request $req, $id)
     {
-	//$rest = Restaurant::find($id);
-	$food_menu = new Food_Menu();
-	$food_menu->restaurant_id = $id;
-	$food_menu->name = $req->new_menu_name;
-	$food_menu->price = $req->new_menu_price;
-	$food_menu->category = $req->new_menu_category;
-	$food_menu->save();
-	if($req->hasFile('new_menu_image') && $req->file('new_menu_image')->isValid()){
-	    $image_file = $req->file('new_menu_image');
-	    $req->file('new_menu_image')->move('img/', $food_menu->id.'_'.$req->file('new_menu_image')->getClientOriginalName());
-	    $food_menu->img_name = $food_menu->id.'_'.$req->file('new_menu_image')->getClientOriginalName(); 
+	if(\Auth::check() && \Auth::user()->user_type == 1)
+	{
+	    $rest = Restaurant::find($id);
+	    if($rest->owner_id != Auth::user()->id)
+	    {
+		return redirect('/');
+	    }
+	    $food_menu = new Food_Menu();
+	    $food_menu->restaurant_id = $id;
+	    $food_menu->name = $req->new_menu_name;
+	    $food_menu->price = $req->new_menu_price;
+	    $food_menu->category = $req->new_menu_category;
+	    $food_menu->save();
+	    if($req->hasFile('new_menu_image') && $req->file('new_menu_image')->isValid()){
+		$image_file = $req->file('new_menu_image');
+		$req->file('new_menu_image')->move('img/', $food_menu->id.'_'.$req->file('new_menu_image')->getClientOriginalName());
+		$food_menu->img_name = $food_menu->id.'_'.$req->file('new_menu_image')->getClientOriginalName(); 
+	    }
+	    $food_menu->save();
+	    return redirect()->back();
 	}
-	$food_menu->save();
-	return redirect()->back();
+	else
+	{
+	     return redirect('/login');
+	}
     }
     public function addRestaurantTable(Request $req, $id)
     {
-	$rest = Restaurant::find($id);
-	$quantity = $req->input('new_num_of_tables');
-	$capacity = $req->input('new_capacity');
-	$booking_fee = $req->input('new_booking_fee');
-	for($i = 0; $i < $quantity; $i++)
+	if(\Auth::check() && \Auth::user()->user_type == 1)
 	{
-	    $table = new RestaurantTable();
-	    $table->restaurant_id = $id;
-	    $table->capacity = $capacity;
-	    $table->booking_fee = $booking_fee;
-	    $table->save();
+	    $rest = Restaurant::find($id);
+	    if($rest->owner_id != Auth::user()->id)
+	    {
+		return redirect('/');
+	    }
+	    $quantity = $req->input('new_num_of_tables');
+	    $capacity = $req->input('new_capacity');
+	    $booking_fee = $req->input('new_booking_fee');
+	    for($i = 0; $i < $quantity; $i++)
+	    {
+		$table = new RestaurantTable();
+		$table->restaurant_id = $id;
+		$table->capacity = $capacity;
+		$table->booking_fee = $booking_fee;
+		$table->save();
+	    }
+	    return redirect('restaurant_info_update/'.$id);
 	}
-	return redirect('restaurant_info_update/'.$id);
+	else
+	{
+	     return redirect('/login');
+	}
+
     }
     public function updateRestaurantTable(Request $req, $table_id)
     {
-	$table = RestaurantTable::find($table_id);
-	$table->capacity = $req->input('capacity');
-	$table->booking_fee = $req->input('booking_fee');
-	$table->save();
-	return redirect()->back();
+	if(\Auth::check() && \Auth::user()->user_type == 1)
+	{
+	    $table = RestaurantTable::find($table_id);
+	    $rest = Restaurant::find($table->restaurant_id);
+	    if($rest->owner_id != Auth::user()->id)
+	    {
+		return redirect('/');
+	    }
+	    $table->capacity = $req->input('capacity');
+	    $table->booking_fee = $req->input('booking_fee');
+	    $table->save();
+	    return redirect()->back();
+	}
+	else
+	{
+	     return redirect('/login');
+	}
     }
 }
